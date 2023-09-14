4a4c1ef55cf9044f8de42a19cc9dd6ff291db2f0
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2023-09-14 13:08:41 +0200

    push

--- chat/views.py
+++ chat/views.py
@@ -4,6 +4,9 @@ from  .models import Message
 from  .models import Chat
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 # Create your views here.
 
 @login_required(login_url='/login/')
@@ -22,7 +25,21 @@ def login_view(request):
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
         if user:
             login(request, user)
-            return HttpResponseRedirect(request.POST.get('redirect'))
+            if redirect == None:
+                return redirect('/login/')
+            else:
+                return HttpResponseRedirect(request.POST.get('redirect'))
         else:
             return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect } )        
     return render(request, 'auth/login.html',  {'redirect': redirect} )
+
+
+def sign_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/chat/')  # Weiterleitung zur Anmeldeseite nach erfolgreicher Registrierung
+    else:
+        form = UserCreationForm()
+    return render(request, 'auth/signup.html', {'form': form})
